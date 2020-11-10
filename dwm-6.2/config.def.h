25b97f25b2d48e4c36ef7e58b4d162157413a473
jesus <jesus_lopez1997@hotmail.com>
2020-11-09 20:33:31 -0400

    arch installation

--- dwm-6.2/config.def.h
+++ dwm-6.2/config.def.h
@@ -1,5 +1,4 @@
 /* See LICENSE file for copyright and license details. */
-
 #include <X11/XF86keysym.h>
 
 /* appearance */
@@ -8,13 +7,13 @@ static const unsigned int gappx     = 5;        /* gaps between windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "monospace:size=10" };
+static const char *fonts[]          = { "source code pro:size=11" };
 static const char dmenufont[]       = "monospace:size=10";
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#ffffff";
 static const char col_gray4[]       = "#ffffff";
-static const char col_cyan[]        = "#000000";
+static const char col_cyan[]        = "#303030";
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
 	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
@@ -22,7 +21,7 @@ static const char *colors[][3]      = {
 };
 
 /* tagging */
-static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
+static const char *tags[] = { "1", "2", "3", "4" };
 
 static const Rule rules[] = {
 	/* xprop(1):
@@ -47,7 +46,7 @@ static const Layout layouts[] = {
 };
 
 /* key definitions */
-#define MODKEY Mod1Mask
+#define MODKEY Mod4Mask
 #define TAGKEYS(KEY,TAG) \
 	{ MODKEY,                       KEY,      view,           {.ui = 1 << TAG} }, \
 	{ MODKEY|ControlMask,           KEY,      toggleview,     {.ui = 1 << TAG} }, \
@@ -60,7 +59,12 @@ static const Layout layouts[] = {
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
-static const char *termcmd[]  = { "st", NULL };
+static const char *termcmd[]       = { "kitty", NULL };
+static const char *open_browser[]  = { "chromium", NULL };
+static const char *open_pcmanfm[]  = { "pcmanfm", NULL };
+static const char *poweroff[]      = { "poweroff", NULL};
+static const char *lockscreen[]    = { "xlock", NULL};
+static const char *rofi_run[]      = { "rofi","-show", "run", NULL};
 
 /* for volume */
 static const char *mutevol[] = { "amixer", "set", "Master", "toggle", NULL };
@@ -69,12 +73,15 @@ static const char *upvol[]   = { "amixer", "set", "Master", "5+",     NULL };
 
 static Key keys[] = {
 	/* modifier                     key        function        argument */
-	{ MODKEY,                       XF86XK_AudioMute,            spawn,          {.v = mutevol } },
+    { MODKEY,                       XF86XK_AudioMute,            spawn,          {.v = mutevol } },
 	{ MODKEY,                       XF86XK_AudioLowerVolume,     spawn,          {.v = downvol } },
 	{ MODKEY,                       XF86XK_AudioRaiseVolume,     spawn,          {.v = upvol   } },
-	{ MODKEY,			XK_w,	   spawn,	   SHCMD("$BROWSER") },
-	{ MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } },
+	{ MODKEY,                       XK_p,      spawn,          {.v = rofi_run } },
+	{ MODKEY,                       XK_5,      spawn,          {.v = open_pcmanfm } },
+	{ MODKEY,                       XK_6,      spawn,          {.v = open_browser } },
 	{ MODKEY|ShiftMask,             XK_Return, spawn,          {.v = termcmd } },
+	{ MODKEY|ControlMask|ShiftMask, XK_End,    spawn,          {.v = poweroff} },
+	{ MODKEY|ControlMask|ShiftMask, XK_l,      spawn,          {.v = lockscreen} },
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
 	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
 	{ MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
